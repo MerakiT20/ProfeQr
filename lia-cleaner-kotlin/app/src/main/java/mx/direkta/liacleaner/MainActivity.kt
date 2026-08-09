adb9dbaf073450a3e1009ebeccdb83539c9b9b09
MerakiT20 <raylop58@gmail.com>
2026-08-09 09:53:19 -0600

    Wire cleaner settings and refresh after Android settings

--- lia-cleaner-kotlin/app/src/main/java/mx/direkta/liacleaner/MainActivity.kt
+++ lia-cleaner-kotlin/app/src/main/java/mx/direkta/liacleaner/MainActivity.kt
@@ -5,20 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import mx.direkta.liacleaner.system.AndroidSystemGatewayImpl
-import mx.direkta.liacleaner.ui.LiaCleanerApp
+import mx.direkta.liacleaner.system.CleanerPreferences
+import mx.direkta.liacleaner.ui.LiaCleanerRoot
 import mx.direkta.liacleaner.ui.theme.LiaCleanerTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var preferences: CleanerPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        preferences = CleanerPreferences(applicationContext)
         val systemGateway = AndroidSystemGatewayImpl(applicationContext)
 
         setContent {
             LiaCleanerTheme {
-                LiaCleanerApp(systemGateway)
+                LiaCleanerRoot(
+                    systemGateway = systemGateway,
+                    preferences = preferences,
+                    onPreferencesChanged = { recreate() }
+                )
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::preferences.isInitialized && preferences.consumeUsageSettingsRefresh()) {
+            window.decorView.post { recreate() }
         }
     }
 }
